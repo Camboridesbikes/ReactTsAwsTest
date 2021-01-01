202962c7b6fe068071542a6a7c5db23880ac843b
Cameron Hattendorf <hattendorfcameron@gmail.com>
2021-01-01 14:32:26 -0800

    first commit

---
+++ src/Components/Index.tsx
@@ -0,0 +1,14 @@
+import React, { ReactElement } from 'react'
+
+interface Props {
+    
+}
+
+export default function Index({}: Props): ReactElement {
+    return (
+        <div>
+            <p>Hello World</p> 
+        </div>
+    )
+}
+
